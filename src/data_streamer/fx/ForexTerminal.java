70885f7a59aefc4a0d30e6a032509c5d86344b55
kugurst <ma2799@gmail.com>
2014-12-22 13:20:00 -0500

    Adding JavaFX to the build path.
    It requres Oracle's JRE/JDK 7u6 or above

--- src/data_streamer/fx/ForexTerminal.java
+++ src/data_streamer/fx/ForexTerminal.java
@@ -1,25 +1,54 @@
 package data_streamer.fx;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.effect.DropShadow;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import data_streamer.Market;
+import data_streamer.fx.view.BarChartAveragesController;
 
 public class ForexTerminal extends Application
 {
-    private BorderPane rootLayout;
-    private Stage      primaryStage;
+    public static final ExecutorService exec = Executors.newCachedThreadPool();
+
+    private BorderPane                  rootLayout;
+    private Stage                       primaryStage;
+    private AnchorPane                  terminalView;
+    private Market                      mkt;
+
+    public Stage getPrimaryStage()
+    {
+        return primaryStage;
+    }
 
     public void initRootLayout()
     {
+        mkt =
+            new Market("src/config/market.config",
+                "src/config/data_config.txt", "./");
+
+        try {
+            mkt.loadConfigs();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        try {
+            mkt.setupTimer();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
@@ -36,12 +65,14 @@ public class ForexTerminal extends Application
         }
     }
 
-    public void showTerminalView() {
+    public void showTerminalView()
+    {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(ForexTerminal.class.getResource("/data_streamer/fx/view/TerminalView.fxml"));
-            AnchorPane terminalView = (AnchorPane) loader.load();
+            loader.setLocation(ForexTerminal.class
+                .getResource("/data_streamer/fx/view/TerminalView.fxml"));
+            terminalView = (AnchorPane) loader.load();
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(terminalView);
@@ -50,11 +81,40 @@ public class ForexTerminal extends Application
         }
     }
 
-    public Stage getPrimaryStage()
+    public void showExchangeBarGraph()
     {
-        return primaryStage;
+        try {
+            // Load the fxml file and create a new stage for the popup.
+            FXMLLoader loader = new FXMLLoader() {};
+            loader
+                .setLocation(ForexTerminal.class
+                    .getResource("/data_streamer/fx/view/BarChartAveragesView.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Set the persons into the controller.
+            BarChartAveragesController controller = loader.getController();
+            // The only child of terminalView is the SplitPane
+            for (Node node : terminalView.getChildren()) {
+                SplitPane pane = (SplitPane) node;
+                for (Node split : pane.getItems()) {
+                    String id = split.getId();
+                    if (id != null && id.equals("barChartPane")) {
+                        // Found the pane, get the scroll pane
+                        AnchorPane aPane = (AnchorPane) split;
+                        ScrollPane sPane =
+                            (ScrollPane) aPane.getChildren().get(0);
+                        sPane.setContent(page);
+                    }
+                }
+            }
+
+            controller.setMarketData(mkt);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     @Override
     public void start(Stage primaryStage)
     {
@@ -64,6 +124,13 @@ public class ForexTerminal extends Application
         initRootLayout();
 
         showTerminalView();
+        showExchangeBarGraph();
+    }
+
+    @Override
+    public void stop()
+    {
+        exec.shutdownNow();
     }
 
     public static void main(String[] args)
